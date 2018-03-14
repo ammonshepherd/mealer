19a956682c155a47f21184bef55219112991aac6
Ammon Shepherd <ammon.shepherd@gmail.com>
2018-03-13 22:27:49 -0400

    Move creation of days to mealer.js and days.json files
    This moves the data about the days cards to a days.json file which for
    now just holds the day of the week and the color of the card. Then
    mealer.js dynamically creates the day cards using the days.json file.
    The index.pug file is updated to not have the lines of code that
    formerly created the day cards, and adds a pug-only comment that the day
    and meal cards are generated in the mealer.js file.

--- public/javascripts/mealer.js
+++ public/javascripts/mealer.js
@@ -20,49 +20,86 @@ $( document ).ready(function() {
     .fail(function(data, textStatus, error){
       var err = textStatus + ", " + error;
       console.log("Request failed: " + err);
-  });
+    });
+
+  $.getJSON("./days.json")
+    .done(function(json) {
+      json.forEach(function(day) {
+        const dayHTML = `
+          <div class="row">
+            <div class="card `+ day.color +`">
+              <div class="card-content">`+ day.name.toUpperCase() +`
+                <div class="day" id="`+ day.name +`"></div>
+              </div>
+            </div>
+          </div>
+          `;
+        $("#week").append(dayHTML); 
+      });
+    })
+    .fail(function(data, textStatus, error){
+      var err = textStatus + ", " + error;
+      console.log("Request failed: " + err);
+    });
 
 });
 
-/* Create the drake object that allows the meals cards to be dropped onto day cards
-* Options: copy = clone the meal card instead of moving the actual meal card
-*          accepts = function to state that only the meals can be dragged, not the day cards
-* on: waits for the drop, then calls the function to add the delete button to the meal card.
-*/
+// Get the dates for the next 7 days. 
 
-var drake = dragula(
-  [
-    document.getElementById("sunday"), 
-    document.getElementById("monday"), 
-    document.getElementById("tuesday"),
-    document.getElementById("wednesday"),
-    document.getElementById("thursday"), 
-    document.getElementById("friday"), 
-    document.getElementById("saturday"), 
-    document.getElementById("meals")
-  ], {
-    copy: function(el, source){
-      return source === document.getElementById("meals");
-    },
-    accepts: function(el, target, source, sibling){
-      return target !== document.getElementById("meals");
-    }
-}).on('drop', addDeleteMealButton);
+// Get the day of the week for tomorrow.
+function getTomorrow() {
+  var today = new Date();
+  var tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate()+1);
 
-function addDeleteMealButton(el, to, from) {
-  // Only add the delete button if it is not already there.
-  if (el.querySelector(".delete-meal-card") === null) {
-    el.insertAdjacentHTML('beforeend', '<div class="card-action delete-meal"><i class="material-icons delete-meal-card red-text" onclick="this.parentNode.parentNode.remove()">delete</i></div>');
-  }
+  var num_tomorrow = tomorrow.getDay();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[num_tomorrow];
 }
 
-// Allow window to scroll when dragging a meal card
-var scroll = autoScroll([ 
-    window,
-    document.querySelector('#week'), document.querySelector('#recipes') 
-  ],{
-    margin: 20,
-    autoScroll: function() {
-      return this.down && drake.dragging;
+
+window.onload = function() {
+
+  /* Create the drake object that allows the meals cards to be dropped onto day cards
+  * Options: copy = clone the meal card instead of moving the actual meal card
+  *          accepts = function to state that only the meals can be dragged, not the day cards
+  * on: waits for the drop, then calls the function to add the delete button to the meal card.
+  */
+
+  var drake = dragula(
+    [
+      document.getElementById("sunday"), 
+      document.getElementById("monday"), 
+      document.getElementById("tuesday"),
+      document.getElementById("wednesday"),
+      document.getElementById("thursday"), 
+      document.getElementById("friday"), 
+      document.getElementById("saturday"), 
+      document.getElementById("meals")
+    ], {
+      copy: function(el, source){
+        return source === document.getElementById("meals");
+      },
+      accepts: function(el, target, source, sibling){
+        return target !== document.getElementById("meals");
+      }
+  }).on('drop', addDeleteMealButton);
+
+  function addDeleteMealButton(el, to, from) {
+    // Only add the delete button if it is not already there.
+    if (el.querySelector(".delete-meal-card") === null) {
+      el.insertAdjacentHTML('beforeend', '<div class="card-action delete-meal"><i class="material-icons delete-meal-card red-text" onclick="this.parentNode.parentNode.remove()">delete</i></div>');
     }
-});
+  }
+
+  // Allow window to scroll when dragging a meal card
+  var scroll = autoScroll([ 
+      window,
+      document.querySelector('#week'), document.querySelector('#recipes') 
+    ],{
+      margin: 20,
+      autoScroll: function() {
+        return this.down && drake.dragging;
+      }
+  });
+}
